557c79e4a68c6a9f9f7e08ffb7bdf9420dbef8ab
tdj4909 <tdj4909@gmail.com>
2025-03-30 22:54:04 +0900

    beachList

--- src/main/java/com/sea/blueNow/BasicController.java
+++ src/main/java/com/sea/blueNow/BasicController.java
@@ -10,4 +10,9 @@ public class BasicController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/beachList")
+    public String beachList(){
+        return "beachList";
+    }
 }
